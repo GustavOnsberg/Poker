62476a5d892829275c4346316726432a696bdc73
ViktorRindom <43568804+ViktorRindom@users.noreply.github.com>
2020-01-08 16:39:57 +0100

    table

--- src/TableComponent.java
+++ src/TableComponent.java
@@ -7,7 +7,15 @@ public class TableComponent extends JPanel {
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawOval(100, 100, 100, 100);
+        int width = (int) (getWidth()*0.6);
+        int height = (int) (getHeight()*0.6);
+        g.setColor(Color.getHSBColor(0.1f, 1, 0.6f));
+        g.fillOval((getWidth()-width)/2, (getHeight()-height)/2, width, height);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(10));
+        g2.setColor(Color.getHSBColor(0.5f, 1, 0.6f));
+        g2.drawOval((getWidth()-width)/2, (getHeight()-height)/2, width, height);
+
 
     }
     private int cardW = 350; // card tile width
