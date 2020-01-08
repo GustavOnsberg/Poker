97c5b73d3fe1adfe7b868096e3534305026811c4
GustavOnsberg <gustavonsberg@gmail.com>
2020-01-08 14:19:28 +0100

    User interface #1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
-public class Main {
-    public static void main(String [] args){
+import javax.swing.*;
+import java.awt.*;
 
+public class Main {
+
+    public static Window window;
+    public static Thread windowThread;
+
+    public static void main(String [] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+
+
+        createWindow(false);
+
+    }
+
+
+
+    public static void createWindow(boolean fullscreen){
+        window = new Window();
+        window.isFullscreen = fullscreen;
+        window.setMinimumSize(new Dimension(1000,600));
+        window.setTitle("Poker");
+        window.setResizable(true);
+        if (fullscreen){
+            window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            window.setUndecorated(true);
+        }
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Thread windowThread = new Thread(window);
+
+        windowThread.start();
     }
 }
--- src/Window.java
+++ src/Window.java
@@ -1,15 +1,146 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
-public class Window extends JFrame implements ActionListener, Runnable{
+public class Window extends JFrame implements ActionListener, Runnable, ChangeListener {
+    boolean isFullscreen;
+
+    int playBtnW = 120;
+    int playBtnH = 40;
+    int playBtnSpace = 10;
+    int sidePanelW = 400;
+
+    Container cp = getContentPane();
+    Dimension lastSize = new Dimension(0,0);
+
+    JPanel gamePanel = new JPanel();
+    JPanel sidePanel = new JPanel();
+    JPanel settingsPanel = new JPanel();
+
+    JButton btnRaise = new JButton("Raise");
+    JButton btnCall = new JButton("Call");
+    JButton btnFold = new JButton("Fold");
+
+    JSlider betSlider = new JSlider(JSlider.HORIZONTAL,50000,2000);
+    JLabel betSliderLabel = new JLabel("$ 10000");
+
+    JButton btnSettingsDeck = new JButton("D");
+    JButton btnSettingsMusic = new JButton("M");
+    JButton btnSettingsSound = new JButton("S");
+    JButton btnSettingsFullscreen = new JButton("F");
+    JButton btnSettingsLeave = new JButton("L");
+
+    int settingsNum = 5;
+
+    public Window(){
+        cp.setLayout(null);
+        cp.add(gamePanel);
+        cp.add(sidePanel);
+
+        betSlider.setMajorTickSpacing(10000);
+        betSlider.setMinorTickSpacing(1000);
+        betSlider.setPaintTicks(true);
+        betSlider.setPaintLabels(true);
+        betSlider.setBackground(Color.YELLOW);
+        betSlider.addChangeListener(this);
+
+        betSliderLabel.setVisible(true);
+
+        gamePanel.setLayout(null);
+        gamePanel.add(btnRaise);
+        gamePanel.add(btnCall);
+        gamePanel.add(btnFold);
+        gamePanel.add(betSlider);
+        gamePanel.add(betSliderLabel);
+        gamePanel.setBackground(Color.GREEN);
+
+        sidePanel.setLayout(null);
+        sidePanel.setBackground(Color.RED);
+        settingsPanel.setVisible(true);
+        settingsPanel.setLayout(null);
+        settingsPanel.setBounds(0,0,sidePanelW,sidePanelW/settingsNum);
+        settingsPanel.setBackground(null);
+        sidePanel.add(settingsPanel);
+        btnSettingsDeck.setBounds(0,0,sidePanelW/settingsNum,sidePanelW/settingsNum);
+        btnSettingsMusic.setBounds(sidePanelW/settingsNum,0,sidePanelW/settingsNum,sidePanelW/settingsNum);
+        btnSettingsSound.setBounds(sidePanelW/settingsNum*2,0,sidePanelW/settingsNum,sidePanelW/settingsNum);
+        btnSettingsFullscreen.setBounds(sidePanelW/settingsNum*3,0,sidePanelW/settingsNum,sidePanelW/settingsNum);
+        btnSettingsLeave.setBounds(sidePanelW/settingsNum*4,0,sidePanelW/settingsNum,sidePanelW/settingsNum);
+
+        settingsPanel.add(btnSettingsDeck);
+        settingsPanel.add(btnSettingsMusic);
+        settingsPanel.add(btnSettingsSound);
+        settingsPanel.add(btnSettingsFullscreen);
+        settingsPanel.add(btnSettingsLeave);
+
+        btnSettingsDeck.addActionListener(this);
+        btnSettingsMusic.addActionListener(this);
+        btnSettingsSound.addActionListener(this);
+        btnSettingsFullscreen.addActionListener(this);
+        btnSettingsLeave.addActionListener(this);
+
+
+
+
+        onSizeChange();
+    }
+
+
+
+
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-
+        if (actionEvent.getSource().equals(btnSettingsFullscreen)){
+            Main.createWindow(!isFullscreen);
+            dispose();
+        }
     }
 
     @Override
     public void run() {
+        while(true){
+            if(!cp.getSize().equals(lastSize)){
+                onSizeChange();
+                lastSize.setSize(cp.getSize());
+                System.out.println("Window has been resized");
+            }
 
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    public void onSizeChange(){
+        gamePanel.setBounds(0, 0, cp.getWidth() - sidePanelW, cp.getHeight());
+        sidePanel.setBounds(cp.getWidth()-sidePanelW,0,sidePanelW, cp.getHeight());
+
+
+        btnRaise.setBounds(gamePanel.getWidth() / 2 - playBtnW * 3 / 2 - playBtnSpace, gamePanel.getHeight() - playBtnH - playBtnSpace, playBtnW, playBtnH);
+        btnCall.setBounds(gamePanel.getWidth() / 2 - playBtnW / 2, gamePanel.getHeight() - playBtnH - playBtnSpace, playBtnW, playBtnH);
+        btnFold.setBounds(gamePanel.getWidth() / 2 + playBtnW / 2 + playBtnSpace, gamePanel.getHeight() - playBtnH - playBtnSpace, playBtnW, playBtnH);
+
+        betSlider.setBounds(50,cp.getHeight() - 2 * playBtnSpace - playBtnH - 50, gamePanel.getWidth() - 100, 50);
+
+        betSliderLabel.setBounds(betSlider.getX() + betSlider.getWidth() * betSlider.getValue() / betSlider.getMaximum(),betSlider.getY() - 120,200,200);
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent changeEvent) {
+        if (changeEvent.getSource().equals(betSlider)){
+            betSliderLabel.setBounds(betSlider.getX() + betSlider.getWidth() * betSlider.getValue() / betSlider.getMaximum(),betSlider.getY() - 120,200,200);
+            if (betSlider.getValue() != betSlider.getMaximum())
+                betSliderLabel.setText("$ "+betSlider.getValue());
+            else
+                betSliderLabel.setText("ALL IN");
+        }
     }
 }
