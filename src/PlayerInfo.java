ac59bc0556e17fa8e015a26f4f7513b319faf882
ViktorRindom <43568804+ViktorRindom@users.noreply.github.com>
2020-01-14 19:17:38 +0100

    anim 4 awsome

--- src/PlayerInfo.java
+++ src/PlayerInfo.java
@@ -1,16 +1,36 @@
 public class PlayerInfo {
+    public DataTypes.CardType card0;
     public DataTypes.CardType card1;
-    public DataTypes.CardType card2;
     public int cash;
 
-    PlayerInfo(DataTypes.CardType card1, DataTypes.CardType card2, int cash){
+    PlayerInfo(DataTypes.CardType card0, DataTypes.CardType card1, int cash){
+        this.card0 = card0;
         this.card1 = card1;
-        this.card2 = card2;
         this.cash = cash;
     }
-
+    public void setCard(int cardId, DataTypes.CardType card){
+        if (cardId == 0) {
+            this.card0 = card;
+        } else {
+            this.card1 = card;
+        }
+    }
+    public void removeCard(int cardId){
+        if (cardId == 0) {
+            this.card0 = DataTypes.CardType.none;
+        } else {
+            this.card1 = DataTypes.CardType.none;
+        }
+    }
     public int getCash() {
         return cash;
+    }
+    public DataTypes.CardType getCard(int cardId){
+        if (cardId == 0) {
+            return this.card0;
+        }else {
+            return this.card1;
+        }
     }
 
 
